33465ffd0daf5d01b3291368d3a9f7f5d67331aa
Frank Rousseau <frank.rousseau@cozycloud.cc>
2016-03-27 23:01:12 +0200

    Make init proper

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 var fs = require('fs');
+var path = require('path');
+var copy = require('cpr');
 
 /*
 This is invoked as a shell script by NPM when the `tiddlywiki` command is typed
@@ -7,34 +9,31 @@ This is invoked as a shell script by NPM when the `tiddlywiki` command is typed
 
 var $tw = require("./boot/boot.js").TiddlyWiki();
 
-var path = '/usr/local/var/cozy/tiddlywiki5/';
-var items = fs.readdirSync(path);
-
+var dirPath = path.join('/', 'usr', 'local', 'var', 'cozy', 'tiddlywiki5');
+var isInfoFile = fs.existsSync(path.join(dirPath, 'tiddlywiki.info'));
 var port = process.env.PORT || "9444";
 
 
-function init() {
-  process.argv.push(path);
-  process.argv = process.argv.concat([
-    '--init',
-    'server'
-  ]);
-
-  $tw.boot.argv = Array.prototype.slice.call(process.argv, 2);
-  $tw.boot.boot();
-  process.argv = process.argv.slice(0, 2);
+function init(callback) {
+  var opts = {
+    overwrite: true,
+    confirm: true
+  };
+  copy('./editions/server', dirPath, opts, function (err) {
+    if (err) return callback(err);
+  });
 }
 
 
 /* Simulation of a proper command line call:
  *
  *     node server.js --server <port> <roottiddler> <rendertype> <servetype>
- *     <username> <password> <host> <pathprefix>
+ *     <username> <password> <host> <dirPathprefix>
  * */
 function startServer() {
-  process.chdir(path);
+  process.chdir(dirPath);
   process.argv = process.argv.concat([
-    path,
+    dirPath,
     '--server',
     port, // port
     '$:/core/save/all', // roottidler
@@ -42,21 +41,29 @@ function startServer() {
     'text/html', // servertype
     '', // username, nothing, auth is handled by Cozy
     '', // password, nothing auth is handled by Cozy
-    '127.0.0.1', // host
-    '/apps/tiddlywiki5' // pathprefix
+    '127.0.0.1' // host
   ]);
   $tw.boot.argv = Array.prototype.slice.call(process.argv, 2);
   $tw.boot.boot();
   $tw.wiki.addTiddler(new $tw.Tiddler({
     title: "$:/config/tiddlyweb/host",
-    text: "http://127.0.0.1/apps/tiddlywiki5"
+    text: "http://127.0.0.1/apps/tiddlywiki5/"
   }));
 }
 
-if(items.length > 0) {
+if (isInfoFile) {
+  console.log('Info file exists');
   startServer();
 } else {
-  init();
-  startServer();
+  console.log('Info file does not exist. Init is required.');
+  init(function (err) {
+    if (err) {
+      console.log('Init failed.');
+      console.log(err);
+    } else {
+      console.log('Init is done.');
+      startServer();
+    }
+  });
 };
 
