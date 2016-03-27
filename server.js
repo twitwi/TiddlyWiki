52b4e64b4989f444da908c4e28e6fa80d43f603b
Frank Rousseau <frank.rousseau@cozycloud.cc>
2016-03-27 16:07:51 +0200

    Create first tiddler when starting the server.

--- server.js
+++ server.js
@@ -13,6 +13,19 @@ var items = fs.readdirSync(path);
 var port = process.env.PORT || "9444";
 
 
+function init() {
+  process.argv.push(path);
+  process.argv = process.argv.concat([
+    '--init',
+    'server'
+  ]);
+
+  $tw.boot.argv = Array.prototype.slice.call(process.argv, 2);
+  $tw.boot.boot();
+  process.argv = process.argv.slice(0, 2);
+}
+
+
 /* Simulation of a proper command line call:
  *
  *     node server.js --server <port> <roottiddler> <rendertype> <servetype>
@@ -20,37 +33,30 @@ var port = process.env.PORT || "9444";
  * */
 function startServer() {
   process.chdir(path);
-  process.argv.push(path);
-  process.argv.push('--server');
-  process.argv.push(port); // port
-  process.argv.push('$:/core/save/all'); // roottidler
-  process.argv.push('text/plain'); // rendertype
-  process.argv.push('text/html'); // servertype
-  process.argv.push(''); // username, nothing, auth is handled by Cozy
-  process.argv.push(''); // password, nothing auth is handled by Cozy
-  process.argv.push('127.0.0.1'); // host
-  process.argv.push('/apps/tiddlywiki5'); // pathprefix
-  console.log(process.argv);
+  process.argv = process.argv.concat([
+    path,
+    '--server',
+    port, // port
+    '$:/core/save/all', // roottidler
+    'text/plain', // rendertype
+    'text/html', // servertype
+    '', // username, nothing, auth is handled by Cozy
+    '', // password, nothing auth is handled by Cozy
+    '127.0.0.1', // host
+    '/apps/tiddlywiki5' // pathprefix
+  ]);
+  $tw.boot.argv = Array.prototype.slice.call(process.argv, 2);
+  $tw.boot.boot();
+  $tw.wiki.addTiddler(new $tw.Tiddler({
+    title: "$:/config/tiddlyweb/host",
+    text: "http://127.0.0.1/apps/tiddlywiki5"
+  }));
 }
 
 if(items.length > 0) {
   startServer();
-  $tw.boot.argv = Array.prototype.slice.call(process.argv, 2);
-  $tw.boot.boot();
-
 } else {
-  process.argv.push(path);
-  process.argv.push('--init');
-  process.argv.push('server');
-
-  $tw.boot.argv = Array.prototype.slice.call(process.argv, 2);
-  $tw.boot.boot();
-
-  process.argv.pop();
-  process.argv.pop();
-  process.argv.pop();
+  init();
   startServer();
-  $tw.boot.argv = Array.prototype.slice.call(process.argv,2);
-  $tw.boot.boot();
 };
 
